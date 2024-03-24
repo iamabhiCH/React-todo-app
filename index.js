c78a4e238fb4873fe931a884055887b87f452ab1
Abhishek Yadav <abhishekay442080@gmail.com>
2024-03-25 00:58:11 +0530

    Making validations for put and post.

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const express = require("express");
+const { createTodo } = require("./types");
 const app = express();
 
 app.use(express.json());
 
 app.post("/todo", function(req, res){
-    
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload);
+    if (!parsedPayload.success) {
+        res.status(411).json({
+            msg: "You sent the wrong inputs",
+        })
+        return;
+    }
 })
 app.get("/todos", function(req, res){
 
 })
 app.put("/completed", function(req, res){
-
+    const updatePayload = req.body;
+    const parsedPayload = createTodo.safeParse(updatePayload);
+    if (!parsedPayload.success) {
+        res.status(411).json({
+            msg: "You sent the wrong inputs",
+        })
+        return;
+    }
 })
