b23f66c20b660afcf64261d5b06645b583f8cc66
Abhishek Yadav <abhishekay442080@gmail.com>
2024-03-24 23:50:25 +0530

    backend project files.

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.post("/todo", function(req, res){
+    
+})
+app.get("/todos", function(req, res){
+
+})
+app.put("/completed", function(req, res){
+
+})
